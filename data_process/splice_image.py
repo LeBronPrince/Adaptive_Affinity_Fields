ed87b90533242aa0b0fc0ef750702d915c1831b0
LeBronPrince <my13020005296@gmail.com>
2019-05-19 16:23:21 +0800

    splice test

--- data_process/splice_image.py
+++ data_process/splice_image.py
@@ -1,26 +1,29 @@
 import numpy as np
 import os
 import cv2
-datapath = '/home/f523/wangyang/segmentation/Vaihingen/Split/test/original_11'
+datapath = '/home/f523/wangyang/segmentation/Adaptive_Affinity_Fields/inference/inference_final/color'
 im_path = '/home/f523/wangyang/segmentation/Vaihingen/ISPRS_semantic_labeling_Vaihingen_ground_truth_COMPLETE/top_mosaic_09cm_area11.tif'
+patch_size = 336
 im = cv2.imread(im_path)
 im = np.array(im)
-w = im.shape(0)
-h = im.shape(1)
-w_num = int(w/336)
-h_num = int(h/336)
+w = im.shape[0]
+h = im.shape[1]
+w_num = int(w/patch_size)
+h_num = int(h/patch_size)
 num = w_num*h_num
-shape = [w_num*336,h_num*336,3]
+shape = [w_num*patch_size,h_num*patch_size,3]
 shape1 = [w,h,3]
 output = np.zeros(shape)
 output1 = np.zeros(shape1)
 data = []
 for root,dirs,files in os.walk(datapath):
-    path = os.join(datapath,files)
-    image = cv2.imread(path)
-    image = np.array(image)
-    data.append(image)
-
+    for file in files:
+        path = os.path.join(root,file)
+        print(path)
+        image = cv2.imread(path)
+        image = np.array(image)
+        data.append(image)
+print(len(data))
 w = h =0
 for i in range(num):
     if i >= w_num:
